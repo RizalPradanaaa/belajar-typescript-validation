bb1301f318e72fb475d18f3c87c50894573a4588
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-04-11 21:53:27 +0700

    Konversi Tipe Data

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -22,4 +22,19 @@ describe("Validation", () => {
     const price = priceSchema.parse(10000);
     console.info(price);
   });
+
+  it("should support data conversion", async () => {
+    const usernameSchema = z.coerce.string().min(3).max(100);
+    const isAdminSchema = z.coerce.boolean();
+    const priceSchema = z.coerce.number().min(1000).max(1000000);
+
+    const username = usernameSchema.parse(12345);
+    console.info(username);
+
+    const isAdmin = isAdminSchema.parse("true");
+    console.info(isAdmin);
+
+    const price = priceSchema.parse("10000");
+    console.info(price);
+  });
 });
